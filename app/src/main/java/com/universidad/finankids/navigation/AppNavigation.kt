e05bb6340e471dd841cd43533c275313fe4485e3
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-04-05 17:11:47 -0500

    Route to AuthScreen.kt

--- app/src/main/java/com/universidad/finankids/navigation/AppNavigation.kt
+++ app/src/main/java/com/universidad/finankids/navigation/AppNavigation.kt
@@ -2,9 +2,12 @@ package com.universidad.finankids.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.universidad.finankids.ui.AuthScreen
 import com.universidad.finankids.ui.MainScreen
 import com.universidad.finankids.ui.SplashScreen
 
@@ -15,11 +18,22 @@ fun AppNavigation(modifier: Modifier = Modifier){
         navController = navController,
         startDestination = AppScreens.SplashScreen.route
     ){
+
         composable(AppScreens.SplashScreen.route) {
             SplashScreen(navController)
         }
+
         composable(AppScreens.MainScreen.route) {
-            MainScreen()
+            MainScreen(navController)
         }
+
+        composable(
+            route = AppScreens.AuthScreen.route,
+            arguments = listOf(navArgument("startInLogin") { type = NavType.BoolType })
+        ) { backStackEntry ->
+            val startInLogin = backStackEntry.arguments?.getBoolean("startInLogin") ?: true
+            AuthScreen(startInLogin)
+        }
+
     }
 }
