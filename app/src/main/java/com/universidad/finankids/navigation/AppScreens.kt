ab5de845373ebddbfecdf69f95ffaa59018e544b
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-04-05 17:12:00 -0500

    object AuthScreen

--- app/src/main/java/com/universidad/finankids/navigation/AppScreens.kt
+++ app/src/main/java/com/universidad/finankids/navigation/AppScreens.kt
@@ -3,5 +3,8 @@ package com.universidad.finankids.navigation
 sealed class  AppScreens(val route: String){
     object SplashScreen: AppScreens(route = "splash_screen")
     object MainScreen: AppScreens(route = "main_screen")
+    object AuthScreen : AppScreens("auth_screen/{startInLogin}") {
+        fun createRoute(startInLogin: Boolean) = "auth_screen/$startInLogin"
+    }
 }
 
