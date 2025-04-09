e9277d33a30b503d3370fae8eca546b6a654f490
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-04-08 20:24:46 -0500

    initial commit

---
+++ app/src/main/java/com/universidad/finankids/events/AuthEvent.kt
@@ -0,0 +1,18 @@
+package com.universidad.finankids.events
+
+sealed class AuthEvent {
+    // Eventos de formulario
+    data class EmailChanged(val email: String) : AuthEvent()
+    data class PasswordChanged(val password: String) : AuthEvent()
+    data class UsernameChanged(val username: String) : AuthEvent()
+    data class TermsAcceptedChanged(val accepted: Boolean) : AuthEvent()
+
+    // Eventos de acciones
+    object Login : AuthEvent()
+    object Register : AuthEvent()
+    object SignInWithGoogle : AuthEvent()
+    object SignInWithFacebook : AuthEvent()
+    object NavigateToRegister : AuthEvent()
+    object NavigateToLogin : AuthEvent()
+    object ForgotPassword : AuthEvent()
+}
---
+++ app/src/main/java/com/universidad/finankids/state/AuthState.kt
@@ -0,0 +1,12 @@
+package com.universidad.finankids.state
+
+data class AuthState(
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val termsAccepted: Boolean = false,
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val isLoginSelected: Boolean = true,
+    val isSuccess: Boolean = false
+)
