e41dcd0c8afb2a2bd068c00acc1f50ff08940d66
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-04-16 12:58:12 -0500

    add new screens

--- app/src/main/java/com/universidad/finankids/navigation/AppScreens.kt
+++ app/src/main/java/com/universidad/finankids/navigation/AppScreens.kt
@@ -8,5 +8,9 @@ sealed class  AppScreens(val route: String){
     }
     object RecoveryScreen: AppScreens(route = "recovery_screen")
     object HomeScreen: AppScreens(route = "home_screen")
+    object ProfileScreen: AppScreens(route = "profile_screen")
+    object TrophyScreen: AppScreens(route = "trophy_screen")
+    object ProgressScreen: AppScreens(route = "progress_screen")
+    object StoreScreen: AppScreens(route = "store_screen")
 }
 
