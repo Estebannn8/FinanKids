6c41c911a7dca82326bfdcb7bc6a28e4b828d204
Luis Esteban <larinomoriano.esteban@gmail.com>
2025-04-29 23:38:44 -0500

    deleted

--- app/src/main/java/com/universidad/finankids/data/model/Actividad.kt
+++
@@ -1,6 +0,0 @@
-package com.universidad.finankids.data.model
-
-data class Actividad(
-    val tipo: String = "",                 // "enseñanza", "completar", "ordenar", "parejas", "seleccion_multiple", "arrastrar"
-    val contenido: Map<String, Any> = emptyMap()  // Contenido específico según el tipo
-)
